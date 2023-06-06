cfc3d31df04de3957868bc06bd3a3c54f5f3361e
Seth Luersen <seth@luersen.com>
2023-06-05 21:12:22 -0700

    update

--- data/world_ddl.sql
+++ data/world_ddl.sql
@@ -1,28 +1,28 @@
-DROP TABLE IF EXISTS tbl_cities;
-DROP TABLE IF EXISTS tbl_states;
-DROP TABLE IF EXISTS tbl_countries;
+drop table if exists tbl_cities;
+drop table if exists tbl_states;
+drop table if exists tbl_countries;
 
 
-CREATE TABLE tbl_cities (
-  city_id INTEGER NOT NULL,
-  city_name varchar(255)  NOT NULL,
+create table tbl_cities (
+  city_id integer not null,
+  city_name varchar(255)  not null,
   city_name_alt varchar(255),
-  state_id INTEGER NOT NULL,
-  state_code varchar(255)  NOT NULL,
-  country_id INTEGER NOT NULL,
-  country_code char(2)  NOT NULL,
-  latitude decimal(10,8) NOT NULL,
-  longitude decimal(11,8) NOT NULL,
-  created_at timestamp NOT NULL DEFAULT '2021-12-31 12:59:59',
-  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  flag SMALLINT NOT NULL DEFAULT '1',
+  state_id integer not null,
+  state_code varchar(255)  not null,
+  country_id integer not null,
+  country_code char(2)  not null,
+  latitude decimal(10,8) not null,
+  longitude decimal(11,8) not null,
+  created_at timestamp not null default '2021-12-31 12:59:59',
+  updated_at timestamp not null default current_timestamp,
+  flag smallint not null default '1',
   wiki_data_id varchar(255),
-  PRIMARY KEY (city_id)
+  primary key (city_id)
 ) ;
 
-CREATE TABLE tbl_countries (
-  country_id INTEGER NOT NULL,
-  country_name varchar(255)  NOT NULL,
+create table tbl_countries (
+  country_id integer not null,
+  country_name varchar(255)  not null,
   country_name_alt varchar(255),
   iso3 char(3)  ,
   numeric_code char(3)  ,
@@ -41,27 +41,27 @@ CREATE TABLE tbl_countries (
   latitude decimal(10,8) ,
   longitude decimal(11,8) ,
   emoji varchar(191) ,
-  emojiU varchar(191) ,
-  created_at timestamp NOT NULL DEFAULT '2021-12-31 12:59:59',
-  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  flag SMALLINT NOT NULL DEFAULT '1',
+  emojiu varchar(191) ,
+  created_at timestamp not null default '2021-12-31 12:59:59',
+  updated_at timestamp not null default current_timestamp,
+  flag smallint not null default '1',
   wiki_data_id varchar(255),
-  PRIMARY KEY (country_id)
+  primary key (country_id)
 ) ;
 
-CREATE TABLE tbl_states (
-  state_id INTEGER NOT NULL,
-  state_name varchar(255)  NOT NULL,
-  country_id INTEGER NOT NULL,
-  country_code char(2)  NOT NULL,
+create table tbl_states (
+  state_id integer not null,
+  state_name varchar(255)  not null,
+  country_id integer not null,
+  country_code char(2)  not null,
   fips_code varchar(255)  ,
   iso2 varchar(255)  ,
   type varchar(191)  ,
   latitude decimal(10,8) ,
   longitude decimal(11,8) ,
-  created_at timestamp NOT NULL DEFAULT '2021-12-31 12:59:59',
-  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  flag SMALLINT NOT NULL DEFAULT '1',
+  created_at timestamp not null default '2021-12-31 12:59:59',
+  updated_at timestamp not null default current_timestamp,
+  flag smallint not null default '1',
   wiki_data_id varchar(255),
-  PRIMARY KEY (state_id)
+  primary key (state_id)
 ) ;
